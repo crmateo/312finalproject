1e4bb2a961782c14bd32833fa6928e9b6b8bc733
Claudia Penney <crpenney@Claudias-MacBook-Pro.local>
2020-05-27 15:14:57 -0700

    Conducted Pearson correlation analysis for CM and SH

--- Scripts/pearsonscor.312final.R
+++ Scripts/pearsonscor.312final.R
@@ -6,7 +6,6 @@ if(!require(devtools)) install.packages("devtools")
 devtools::install_github("kassambara/ggpubr")
 
 #Loading CM & PY data
-
 CM.PY <- read.csv("Data/CM.PY_yr_ct.csv")
 head(CM.PY)
 
@@ -22,3 +21,17 @@ CM.PY.cor.test <- cor.test(CM.PY$PY_ct, CM.PY$CM_ct, method = "pearson")
 #Correlation test interpretation
 CM.PY.cor.test$p.value
 CM.PY.cor.test$estimate
+
+#Loading CM & SH data
+CM.SH <- read.csv("Data/CM.SH_yr_ct.csv")
+head(CM.SH)
+
+#SH Q-Q plot
+ggqqplot(CM.SH$SH_ct, ylab = "Shrimp Count")
+
+#Correlation test
+CM.SH.cor.test <- cor.test(CM.SH$SH_ct, CM.SH$CM_ct, method = "pearson")
+
+#Correlation test interpretation
+CM.SH.cor.test$p.value
+CM.SH.cor.test$estimate
